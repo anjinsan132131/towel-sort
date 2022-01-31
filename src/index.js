437ae019aa0a4082dd18d559fab8453665639980
anjinsan132131 <anjinsan132131@gmail.com>
2022-01-31 22:11:00 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {
+    return [];
+  } 
+
+  return matrix.reduce((acc, elem, index) => {
+      if (index % 2 == 0) {
+          acc.push(...elem);
+      } else {
+          acc.push(...elem.reverse());
+      } 
+      return acc;
+  }, []);
 }
+
+
